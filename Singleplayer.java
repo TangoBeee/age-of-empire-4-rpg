3d91e12d2a10d9b73544f241670eb226f9ca43ea
TangoBee <71539314+TangoBeee@users.noreply.github.com>
2022-05-07 10:34:19 +0530

    Update Singleplayer.java

--- Singleplayer.java
+++ Singleplayer.java
@@ -100,26 +100,75 @@ public class Singleplayer {
 
     public void abbasid_Dynasty() throws InterruptedException {
 //        System.out.println("ad");
-        System.out.println("""
-                Enjoy The Game! :)
-                
-                1. Earn Resource
-                2. Make Army
-                3. Attack
-                4. Defend
-                5. Quit
-                """);
-        for(int i=1; i > 0; i++) {
-            int food = 0;
-            int wood = 0;
-            int gold = 0;
-            int stone = 0;
+        System.out.println("Enjoy The Game! :)");
+        while (true) {
+            System.out.println("""
+                    1. Earn Resource  
+                    2. Make Army  
+                    3. Attack    
+                    4. Defend   
+                    5. Quit 
+                    """);
+            Scanner input = new Scanner(System.in);
+            int j = input.nextInt();
+            if(j == 1) {
+                for (int i = 1; i == 1; i++) {
+                    int food = 0;
+                    int wood = 0;
+                    int gold = 0;
+                    int stone = 0;
 
-            Thread.sleep(10000);
-            food=+100;
-            wood=+100;
-            gold=+100;
-            stone=+50;
+                    Thread.sleep(7000);
+                    System.out.println("Please wait few seconds");
+                    food = +100;
+                    wood = +100;
+                    gold = +100;
+                    stone = +50;
+                    System.out.print("Generating resources");
+                    Thread.sleep(1000);
+                    System.out.print(".");
+                    Thread.sleep(1000);
+                    System.out.print(".");
+                    Thread.sleep(1000);
+                    System.out.print(".");
+                    System.out.println();
+                    System.out.println("You have now " + food + " food, " + wood + " wood, " + gold + " gold, " + " and " + stone + " Stone");
+                }
+            }
+
+            else if(j == 2) {
+                System.out.println("Making army...");
+            }
+
+            else if(j == 3) {
+                System.out.println("Attacking...");
+            }
+
+            else if(j == 4) {
+                System.out.println("Defending...");
+            }
+
+            else if(j == 5) {
+                System.out.println("Are you sure? This match will count as surrender y/n :- ");
+                Scanner input1 = new Scanner(System.in);
+                char k = input1.next().charAt(0);
+                if(k == 'y') {
+                    break;
+                }
+                else if(k == 'n') {
+                    continue;
+                }
+                else {
+                    System.out.println("Wrong input!");
+                    continue;
+                }
+            }
+
+            else {
+                System.out.println("Wrong input!");
+                continue;
+            }
+
         }
 
     }
